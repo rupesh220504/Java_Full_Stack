908ae0860c048ab8b06b66f1ef8214f8bc82760a
Rupesh <210937504+rupesh220504@users.noreply.github.com>
2025-12-27 15:42:25 +0530

    Commit

--- priyadarshini/src/Day12/Signatures.java
+++ priyadarshini/src/Day12/Signatures.java
@@ -6,9 +6,10 @@ public class Signatures {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Rupesh");
-        Signatures.main("Rupesh2",20);
+        String ar[]= {"Arsh","Abc"};
+        Signatures.main(ar,20);
 	}
-	public static void main(String args,int a) {
+	public static void main(String[] args,int a) {
 		// TODO Auto-generated method stub
 		System.out.println(args);
         System.out.println(a);
